767b01d12d795214c2b14dff9030e0ee3344b77d
muhddanishaiman <112624478+muhddanishaiman@users.noreply.github.com>
2025-09-09 18:31:46 -0400

    Updated  vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "/front-end-apps-with-react",
   plugins: [react()],
 })
+
